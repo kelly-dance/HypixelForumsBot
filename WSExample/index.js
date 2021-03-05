54a951e9c20a3ce8b44389732b8b9097885faebc
McPqndq <kellybob3737@gmail.com>
2021-03-05 15:42:02 -0600

    updated wsexmaple

--- WSExample/index.js
+++ WSExample/index.js
@@ -4,9 +4,14 @@ const ws = new WebSocket('wss://hf.mcpqndq.dev/');
 
 ws.on('open', () => {
   console.log('Connected')
+  const interval = setInterval(() => ws.send('ping'), 30e3)
+
+  ws.on('message', msg => {
+    const data = JSON.parse(msg);
+    if(data.type === 'post') console.log(data.data);
+  });
+
+  ws.on('close', () => clearInterval(interval));
 });
 
-ws.on('message', msg => {
-  const data = JSON.parse(msg);
-  console.log(data);
-});
+
