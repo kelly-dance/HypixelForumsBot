c04cdcc115a7f07313db1d0608d4cd93c6462537
WiViW <wiviw2221212@gmail.com>
2021-10-20 16:29:12 -0500

    Added Pixel Party to feeds.ts

--- listener/src/feeds.ts
+++ listener/src/feeds.ts
@@ -109,6 +109,11 @@ const prototype: Tag = {
       name: 'TowerWars',
       url: "https://hypixel.net/forums/game-towerwars.156/",
     }
+    {
+      id: 'pixelparty',
+      name: 'Pixel Party',
+      url: "https://hypixel.net/forums/pixel-party/",
+    }
   ]
 }
 
